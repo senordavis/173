317df6887d8d5851ac22f416a524462e6d8702df
Mike Davis <mike@mdavis.in>
2017-01-09 07:24:36 -0500

    Updated tawk.to script

---
+++ 80321/assets/js/min/tawk-min.js
@@ -0,0 +1,1 @@
+var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date;!function(){var t=document.createElement("script"),e=document.getElementsByTagName("script")[0];t.async=!0,t.src="https://embed.tawk.to/568bf16089864a5c73b44523/default",t.charset="UTF-8",t.setAttribute("crossorigin","*"),e.parentNode.insertBefore(t,e)}();
