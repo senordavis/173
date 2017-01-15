e3c89f593f038a9efc532227ca2e1451f00d7206
Mike Davis <mike@mdavis.in>
2017-01-15 16:36:32 -0500

    Putting together assignments and assignment schedule

---
+++ index.php
@@ -0,0 +1,30 @@
+<? 
+    date_default_timezone_set("America/Detroit");
+    $currentDate = date("mdY");
+
+    if ($currentDate >= "01092017" and $currentDate <= "01142017"){header("Location: https://mdavis.in/173/d2l/80321/");}
+    elseif ($currentDate >= "01152017" and $currentDate <= "01212017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-02.html");}
+    elseif ($currentDate >= "01222017" and $currentDate <= "01282017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-03.html");}
+    elseif ($currentDate >= "01292017" and $currentDate <= "02042017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-04.html");}
+    
+    elseif ($currentDate >= "02052017" and $currentDate <= "02112017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-05.html");}
+    elseif ($currentDate >= "02122017" and $currentDate <= "02182017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-06.html");}
+    elseif ($currentDate >= "02192017" and $currentDate <= "02252017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-07.html");}
+    elseif ($currentDate >= "02262017" and $currentDate <= "03042017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-08.html");}
+    
+    elseif ($currentDate >= "03052017" and $currentDate <= "03112017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/spring-break.html");}
+    
+    elseif ($currentDate >= "03122017" and $currentDate <= "03182017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-09.html");}
+    elseif ($currentDate >= "03192017" and $currentDate <= "03252017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-10.html");}
+    elseif ($currentDate >= "03262017" and $currentDate <= "04012017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-11.html");}
+    
+    elseif ($currentDate >= "04022017" and $currentDate <= "04082017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-12.html");}
+    elseif ($currentDate >= "04092017" and $currentDate <= "04152017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-13.html");}
+    elseif ($currentDate >= "04162017" and $currentDate <= "04222017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-14.html");}
+    elseif ($currentDate >= "04232017" and $currentDate <= "04292017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-14.html");}
+    elseif ($currentDate >= "04302017" and $currentDate <= "05062017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/week-16.html");}
+    elseif ($currentDate >= "05072017"){header("Location: https://mdavis.in/173/d2l/80321/content/weeks/thank-you.html");}
+    
+    else {header("Location: https://mdavis.in/173/d2l/80321/");}
+
+?>
